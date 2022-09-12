f05c9b08555c8d4ccf4e88a19ea4c9027702286c
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2022-09-12 16:37:23 -0400

    Ajout du premier fichier de test
    Création d'un fichier pour tester javascript avec node

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log('Ceci est un test');
+const a = 2;
+const b = 3;
+
+if (a + b === 5) {
+    console.log('Ça fonctionne!');
+} else {
+    console.log('Ça ne fonctionne pas! Erreur!');
+}
