cb827de6febd4de616379595a00c1f2306fac54a
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2022-09-12 17:40:52 -0400

    Ajout des plugins
    installation de fetch and inquirer + test stations BXI Geojson

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-console.log('Ceci est un test');
+const fetch = require('node-fetch');
+
+/*console.log('Ceci est un test');
 const a = 2;
 const b = 3;
 
@@ -6,4 +8,12 @@ if (a + b === 5) {
     console.log('Ça fonctionne!');
 } else {
     console.log('Ça ne fonctionne pas! Erreur!');
-}
+}*/
+
+const stationsBixiUrl = "https://www3.longueuil.quebec/sites/longueuil/files/donnees_ouvertes/stationsbixi.json";
+fetch(stationsBixiUrl).then(function(response) {
+    response.json().then(function(jsonResponse) {
+        console.log(JSON.stringify(jsonResponse));
+        process.exit();
+    });
+});
